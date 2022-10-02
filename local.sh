3250b5062041fed25dc9542cf73b71e2dec09528
Dayana Alejandro <dayana200120@gmail.com>
2022-10-02 14:44:41 -0700

    set up new flask app to connect with home page and modified local.sh to call new flask app

--- local.sh
+++ local.sh
@@ -2,4 +2,5 @@
 
 # run our server locally:
 # PYTHONPATH=$(pwd):$PYTHONPATH
-FLASK_APP=server.endpoints flask run --host=127.0.0.1 --port=8000
+# FLASK_APP=server.endpoints flask run --host=127.0.0.1 --port=8000
+FLASK_APP=main flask run --host=127.0.0.1 --port=8000
---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+This is the file containing all of the endpoints for our flask app.
+The endpoint called `endpoints` will return all available endpoints.
+"""
+
+from flask import Flask, render_template
+# from flask_restx import Resource, Api
+# import db.db as db
+
+app = Flask(__name__)
+# api = Api(app)
+
+
+@app.route("/")
+@app.route("/home", methods=['GET','POST'])
+def home():
+    """
+    The `get()` method will return a list of available endpoints.
+    """
+    return render_template("home.html")
