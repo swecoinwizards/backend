fe1a758c0367970047916e040ae1b6d8509ff696
aruuuke <47250679+aruuuke@users.noreply.github.com>
2022-10-02 16:41:09 -0400

    added 3 functions to interact with db later

--- db/db.py
+++ db/db.py
@@ -10,3 +10,22 @@ def fetch_pets():
     A function to return all pets in the data store.
     """
     return {"tigers": 2, "lions": 3, "zebras": 1}
+
+
+def get_accounts():
+    """""
+    Get a list of accounts from the database
+    """""
+    return
+
+def add_account():
+    """""
+    Add a new account to the database
+    """""
+    return
+
+def delete_account():
+    """""
+    Remove an account from the database
+    """""
+    return
